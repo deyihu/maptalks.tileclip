e0328b4750cf31481d9c57a17a95e2727721d49c
deyihu <mehudeyi@163.com>
2024-11-04 18:42:32 +0800

    fetch tile add headers

--- src/tileget.js
+++ src/tileget.js
@@ -1,12 +1,19 @@
 import { getCanvas, imageFilter } from './cavnas';
 
+const headers = {
+    'accept': 'image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'
+};
+
 export function getTile(url, options = {}) {
     return new Promise((resolve, reject) => {
         if (!url) {
             reject(new Error('url is null'));
             return;
         }
-        fetch(url).then(res => res.blob()).then(blob => createImageBitmap(blob)).then(imagebit => {
+        fetch(url, {
+            headers
+        }).then(res => res.blob()).then(blob => createImageBitmap(blob)).then(imagebit => {
             const filter = options.filter;
             if (filter) {
                 const canvas = getCanvas();
